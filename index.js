aefa293df2e67893fdffe1d3f467c17635a2298f
tandi3 <ysoyucho@gmail.com>
2025-03-18 19:30:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const ramens = [
   { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "https://moringa.instructure.com/courses/967/files/517801/preview", rating: 5, comment: "Sweet!" },
-  { id: 2, name: "Naruto Ramen", restaurant: "Mitsuyado Se", image: "https://moringa.instructure.com/courses/967/files/517800/preview", rating: 4, comment: "Bursts with flavor!" },
-  { id: 3, name: "Nirvana Ramen", restaurant: "Ramen Nagi", image: "https://moringa.instructure.com/courses/967/files/517799/preview", rating: 5, comment: "Taste so good!" },
-  { id: 4, name: "Kojiro Ramen", restaurant: "Ramen Kojiro", image: "https://moringa.instructure.com/courses/967/files/517798/preview", rating: 3, comment: "Too much soup in the meal." },
-  { id: 5, name: "Gyukotsu Ramen", restaurant: "Ramen Nagi", image: "https://moringa.instructure.com/courses/967/files/517797/preview", rating: 2, comment: "Not my favorite." }
+  { id: 2, name: "Naruto Ramen", restaurant: "Mitsuyado Se", image: "https://moringa.instructure.com/courses/967/files/517800/preview", rating: 4, comment: "Delicioso!" },
+  { id: 3, name: "Nirvana Ramen", restaurant: "Ramen Nagi", image: "https://moringa.instructure.com/courses/967/files/517799/preview", rating: 5, comment: "This dish is so rich in flavor!" },
+  { id: 4, name: "Kojiro Ramen", restaurant: "Ramen Kojiro", image: "https://moringa.instructure.com/courses/967/files/517798/preview", rating: 3, comment: "Too much broth." },
+  { id: 5, name: "Gyukotsu Ramen", restaurant: "Ramen Nagi", image: "https://moringa.instructure.com/courses/967/files/517797/preview", rating: 2, comment: "Not a fan of this one." }
 ];
  
   document.addEventListener("DOMContentLoaded", () => {
